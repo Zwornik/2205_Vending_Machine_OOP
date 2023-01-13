2c9b519706cef802c09cf7725b43cab0cbaeb02b
Zwornik <kruszelnickim@gmail.com>
2023-01-13 15:35:18 +0000

    money and storage classes done

---
+++ money.py
@@ -0,0 +1,62 @@
+class Money:
+    INIT_NOMINATIONS = {200: 6, 100: 6, 50: 0, 20: 2, 10: 1, 5: 3, 2: 2, 1: 2, }  # initial nominations machine contains
+
+    def __init__(self):
+        self.money_in = Money.INIT_NOMINATIONS.copy()  # Coins container, stores info about nominations in the machine
+        self.initial_value = self.amount_in()  # Money in machine at the beginning of the day
+
+    def coins_in(self):  # list nominations possessed
+        print("Nomination   Pieces")
+        [(print("{0:>7}    {1:>6}".format(key, value))) for key, value in self.money_in.items()]
+        print("TOTAL        {0:.2f} GBP".format(self.amount_in() / 100))
+        return self.money_in
+
+    def amount_in(self):  # print amount possessed
+        amount = 0  # total value of all coins in the machine
+        for key, value in self.money_in.items():
+            amount += key * value
+        return amount
+
+    def reset_bank(self):  # reset to initial state
+        self.money_in = Money.INIT_NOMINATIONS.copy()  # Coins container, stores info about nominations in the machine
+
+    def add_money(self, cash):  # add user money, e.g. cash = {1:3, 10:2, 100:1}
+        for i in cash:
+            self.money_in[i] += cash[i]
+
+    def income_today(self):  # calculate income since last reset
+        return self.initial_value - self.amount_in()
+
+    def return_change(self, user_amount, basket_value):  # return change after payment e.g. cash = {1:3, 10:2, 100:1},
+        # basket = {'CHOCOLATE': 1, 'APPLE': 3, }, user money
+        change = user_amount - basket_value  # change to return to user
+        unavailable_coins = []
+        change_coins = {}  # Coins to be returned
+        change_value = 0  # Value of all returned coins
+        returned = True  # Flags if full amount was returned
+        for i in self.money_in:  # "i" is a coin nominal
+
+            if change == i:  # Return change of a single coin
+                change -= i  # Reduce balance to return
+                self.money_in[i] -= 1  # Subtracts coin from coins container
+                change_coins[i] = 1  # Add coin to be returned
+                return change_coins
+
+            elif change > i:  # return change of more than a single coins
+                quotient = change // i
+                while quotient > self.money_in[i]:  # check if coins are available
+                    quotient -= 1
+                    # if i not in unavailable_coins:
+                    #     unavailable_coins.append(i)  # adds unavailable coin to list of unavailable coins
+                    #     print(unavailable_coins)
+                if quotient > 0:
+                    change -= i * quotient  # reduces money to return by number of available nominal
+                    self.money_in[i] -= quotient  # subtracts coins from coins container
+                    change_coins[i] = quotient  # Add coins to be returned
+            print(change)
+        for k, v in change_coins.items():  # Calculate value of all returned coins
+            change_value += k * v
+        if change:  # Check if full amount was returned
+            returned = False
+        return change_coins, change_value, returned  # e.g. ({50: 1, 20: 2, 5: 1, 1:3}, 78, True)
+
--- storage.py
+++ storage.py
@@ -1,50 +1,37 @@
-print("111111111111111")
-
 class Storage:
+    SNACK_NAMES = {'1': 'CHOCOLATE', '2': 'MUESLI BAR', '3': 'APPLE', '4': 'POPCORN', '5': 'CHEESE PUFFS'}  # user
+                    # selection number assigned to snack names
     snacks_in = dict
 
     def auto_supply(self):  # automatically insert snack supply
         self.snacks_in = {'CHOCOLATE': 5, 'MUESLI BAR': 5, 'APPLE': 5, 'POPCORN': 5,
                           'CHEESE PUFFS': 5}
 
-    def empty_storage(self):
+    def empty_storage(self):  # Empty snack storage
         self.snacks_in = {'CHOCOLATE': 0, 'MUESLI BAR': 0, 'APPLE': 0, 'POPCORN': 0,
                           'CHEESE PUFFS': 0}
 
-    def load_snacks(self):
-        self.empty_storage()
-        for i in self.snacks_in:
-            while True:
-                sanck_no = input(f'How many {i}S are you adding?  ')
-                if sanck_no.isdigit():  # Verifying if user typed integer
-                    self.snacks_in[i] = int(sanck_no)  # Loading snacks to machine
-                    break
-                else:
-                    print('Wrong input. Please type integer number.')
-        print(f'You have loaded: {self.snacks_in}')
+    def load_snacks(self, snack_name, quantity):  # Load snacks to the machine(inventory)
+        self.snacks_in[snack_name] += quantity
 
-    def snack_inventory(self):
+    def snack_inventory(self):  # Return snack inventory
         print(self.snacks_in)
         return self.snacks_in
 
-    def check_snack(self, snack_no):
-        if self.snacks_in[snacksNames[snack_no]]:
-            return True
-        return False
+    def check_snack(self, snack_no):  # Returns count of selected snack in the machine
+        return self.snacks_in[Storage.SNACK_NAMES[snack_no]]
 
-    def give_snacks(self, basket):
+    def give_snacks(self, basket):  # Deduct basket snacks from snacks inventory
         for snack in basket.keys():
             self.snacks_in[snack] -= basket[snack]
 
-print("22222222222222")
 
-# basket = {'CHOCOLATE': 0, 'MUESLI BAR': 0, 'APPLE': 1, 'POPCORN': 3,
-#             'CHEESE PUFFS': 5}
-# supply = Storage()
-# supply.auto_supply()
-# print(supply.snacks_in)
-# print(supply.give_snacks(basket))
-
+basket = {'CHOCOLATE': 0, 'MUESLI BAR': 0, 'APPLE': 1, 'POPCORN': 3,
+          'CHEESE PUFFS': 5}
+supply = Storage()
+supply.auto_supply()
+print(supply.snacks_in)
+print(supply.give_snacks(basket))
 
 # if __name__ == '__main__':
 #     main()
